7d94c3d68d664bf8d8ddf87a3c9a58fab7c03fb2
mingyuan.li <mingyuan.li@linksfield.net>
2022-03-29 15:46:05 +0800

    add check list

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -27,19 +27,56 @@
     <p>1111111111111111</p>
     <p>1111111111111111</p>
   </ShowMore>
+
+  <div class="check-list">
+    <p
+      v-for="s in showList"
+      :key="s"
+      :class="checkList.includes(s.key) ? 'active' : ''"
+      @click="handleCheck(s)"
+      >{{ s.key }}:{{ s.value }}</p
+    >
+  </div>
+
   <van-button type="primary" @click="openLink">跳转Login</van-button>
 </template>
 
 <script setup>
   import { useRouter } from 'vue-router';
   import ShowMore from '@/components/ShowMore.vue';
+  import { ref } from 'vue';
   const router = useRouter();
-  const msg = 'home';
-  console.log(msg);
+
+  const showList = [
+    { key: 'A', value: '1111' },
+    { key: 'B', value: '2222' },
+    { key: 'C', value: '3333' },
+    { key: 'D', value: '4444' },
+  ];
+  const checkList = ref([]);
+
+  const handleCheck = (s) => {
+    const index = checkList.value.indexOf(s.key);
+    if (index > -1) {
+      checkList.value.splice(index);
+    } else {
+      checkList.value.push(s.key);
+    }
+    console.log(checkList.value)
+  };
 
   const openLink = () => {
     router.push('/login');
   };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+  .check-list {
+    p {
+      cursor: pointer;
+      &.active {
+        color: red;
+      }
+    }
+  }
+</style>
